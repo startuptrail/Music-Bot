9680b00876c02b1707400e6bda287aa21b7940a8
startuptrail <pranavmc1204@gmail.com>
2025-01-07 20:18:40 +0530

    config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["1234512256344002650", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   musicardTheme:"themes16", //Goes from themes1 to themes19
-  activityName: "You", // This is bot status Write Anything here 
-  activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  activityName: "Music", // This is bot status Write Anything here 
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/e76xPAFUzp",
   CheckmarkIcon: "https://cdn.discordapp.com/emojis/819446784647757834.gif",
   MusicIcon:"https://cdn.discordapp.com/emojis/763415718271385610.gif",
   embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
@@ -17,10 +17,10 @@ module.exports = {
 
   nodes: [
     {
-        name: "Server",
-        host: "nodev4.fypmoon.org",
+        name: "Koi Node V4",
+        host: "lavav4.prplmoe.me",
         port: 1118,
-        password: "fypmoon.org",
+        password: "prplmoe.me,
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
